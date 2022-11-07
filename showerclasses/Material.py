41c5914055d71923010761daf3a745cf11f29758
twnelson0 <trevor.w.nelson1@gmail.com>
2022-11-07 16:21:57 -0600

    Initial incomplete skeletons of the classes added

---
+++ showerclasses/Material.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+@dataclass
+class Material:
+"""Material Object 
+
+"""
+    length: float
+    material: str
+
+    def len_x0(self):
+        """Return length of material in units of radiation lengths
+        """
+        return length/radlen_cm(self) #Length in units of radiation length
+
+    def radlen_cm(self):
+        """Return radiaiton length of material in cm
+        """
+        return 41.31 #Radiation Length in cm (polystyrene)
+    
+    def radlen(self):
+        """Return radiaiton length of material in g cm^-2
+        """
+        return 43.79 #g cm^-2
+
+    def ECrit(self):
+        """Return Critical Energy in MeV
+        """
+        return 93.11 #MeV (polystyrene)
+
+
---
+++ showerclasses/Shower.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from Material import Material
+import numpy as np
+
+@dataclass
+class Shower:
+"""Shower Object
+
+"""
+    surface : Material
+    initial_e : float
+    shower_state = np.array([(initial_e,11)])
+    
+    def size(self):
+        """Return the number of particles currently in the shower
+        """
+        return len(shower_state)
+
+    def clear_parent(self,int: indx):
+        """Clear parent particle from shower
+        """
+        pass
+
+    def propagate(self):
+        """Propoagate shower
+        """
+        pass
+
+    def clear_part(self,indx):
+        """Clear particle from the shower (because it has left the material)
+        """
+        pass
+
+
+    def lepton_num(self):
+        """Return lepton number from shower
+        """
+        return None
