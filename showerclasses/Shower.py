add43682cd84e46645ecff2a4f46e01373a43de4
twnelson0 <trevor.w.nelson1@gmail.com>
2022-11-07 17:06:05 -0600

    Small adjustment to shower_state

--- showerclasses/Shower.py
+++ showerclasses/Shower.py
@@ -5,11 +5,13 @@ import numpy as np
 @dataclass
 class Shower:
 """Shower Object
+Contains several methods for the showering and a shower state object
+shower_state = [(E,Pos,particle type)]
 
 """
     surface : Material
     initial_e : float
-    shower_state = np.array([(initial_e,11)])
+    shower_state = np.array([(initial_e,0.0,11)])
     
     def size(self):
         """Return the number of particles currently in the shower
