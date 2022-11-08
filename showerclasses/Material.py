8386af9ba055215d23daf8ed94b27db2f72e468f
twnelson0 <trevor.w.nelson1@gmail.com>
2022-11-08 11:31:21 -0600

    Added a couple of methods to Material and Shower, expanded the propagate method in Shower

--- showerclasses/Material.py
+++ showerclasses/Material.py
@@ -2,16 +2,31 @@ from dataclasses import dataclass
 
 @dataclass
 class Material:
-"""Material Object 
+    """Material Object 
 
-"""
+    """
     length: float
     material: str
+
+    def len(self):
+        """Return length of the material in cm
+        """
+        return self.length
+
+    def dens(self):
+        """Retrun density of the material in g/cm^3
+        """
+        return 1
 
     def len_x0(self):
         """Return length of material in units of radiation lengths
         """
-        return length/radlen_cm(self) #Length in units of radiation length
+        return self.length/self.radlen_cm() #Length in units of radiation length
+
+    def dist_to_x0(self, x : float):
+        """Return a distance in radiation lengths
+        """
+        return x/self.radlen_cm()
 
     def radlen_cm(self):
         """Return radiaiton length of material in cm
--- showerclasses/Shower.py
+++ showerclasses/Shower.py
@@ -4,74 +4,102 @@ import numpy as np
 
 @dataclass
 class Shower:
-"""Shower Object
-Contains several methods for the showering and a shower state object
-shower_state = [(E,Pos,particle type)]
-
-"""
+    """Shower Object
+    Contains several methods for the showering and a shower state object
+    shower_state = [(E,Pos,particle type/id)]
+    """
     surface : Material
     initial_e : float
-    shower_state = np.array([(initial_e,0.0,11)])
+
+
+    def __post_init__(self):
+        shower_state = np.array([(initial_e,0.0,11)])
+        e_loss = 0
     
     def size(self):
         """Return the number of particles currently in the shower
         """
-        return len(shower_state)
+        return len(self.shower_state)
 
-    def clear_parent(self,indx: int):
-        """Clear parent particle from shower
+
+    def crnt_shower(self):
+        """Return current shower state
         """
-        pass
+        return shower_state
 
-    def ionization_loss(self,delta_x):
-        return None
+    def beta(self,E : float,mass = 0.511):
+        """Obtain the propagation speed of an electron/positron
+        """
+        return np.sqrt(1 - mass**2/E**2)
+
+    def ionization_loss(self,delta_x : float, beta : float):
+        """Determine energy loss of an electron/positron due to ionization
+        Here delta_x is taken to be in cm and dE/dx is assumed to be constant for showering particles
+        """
+        return self.surface.dens()*2*delta_x*1/beta**2
 
     def propagate(self):
         """Propoagate shower
         Method will modify the shower class by propagating each of the contents of the shower
         """
         part_indx = 0
-        for part in shower_state:
-            prop_len = surface.radlen()
-            if (abs(part[2]) == 11 && part[0] > surface.e_crit()):
-                print("Bremsstrahlung")
-                shower_state = np.append(shower_state,(part[0]/2, part[1] + prop_len),11)
-                shower_state = np.append(shower_state,(part[0]/2, part[1] + prop_len),22)
+        for part in self.shower_state:
+            if (part[2] == 11):
+                prop_len = surface.radlen()
+                if (abs(part[0] > surface.e_crit())):
+                    print("Bremsstrahlung")
+                    self.shower_state = np.append(self.shower_state,(part[0]/2, part[1] + prop_len),11)
+                    self.shower_state = np.append(self.shower_state,(part[0]/2, part[1] + prop_len),22)
 
-                #Add check for shower leaks after each new particle is generated
+                    #Shower Leakage
+                    if (prop_len > surface.len()):
+                        print("Shower Leakage")
+                        self.shower_state = np.delete(self.shower_state,self.size() - 1)
+                        self.shower_state = np.delete(self.shower_state,self.size() - 1)
 
-                #Remove parent particle
-                shower_state = np.delete(shower_state,0)
-                part_indx += 1
-            elif(abs(part[2]) == 11 && part[0] < surface.e_crit()):
-                print("electron/positron is no longer showering")
-                shower_state = np.delete(shower_state,0)
+                    #Remove parent particle
+                    self.shower_state = np.delete(self.shower_state,0)
+                    part_indx += 1
 
-            if(part[2] == 22 && part[0] > 2*0.511):
-                print("Pair Produce")
-                shower_state = np.append(shower_state,(part[0]/2, part[1] + prop_len),11)
-                shower_state = np.append(shower_state,(part[0]/2, part[1] + prop_len),-11)
+                elif(part[0] < surface.e_crit()):
+                    print("electron/positron is no longer showering")
+                    self.shower_state = np.delete(self.shower_state,0)
 
-                #Remove parent particle
-                shower_state = np.delete(shower_state,0)
-                part_indx += 1
-            elif(part[2] == 22 && part[0] < 2*0.511):
-                print("Photon is no longer showering")
-                shower_state = np.delete(shower_state,0)
+            if (part[2] == 22):
+                if(part[0] > 2*0.511):
+                    print("Pair Produce")
+                    self.shower_state = np.append(self.shower_state,(part[0]/2, part[1] + prop_len),11)
+                    self.shower_state = np.append(self.shower_state,(part[0]/2, part[1] + prop_len),-11)
 
+                    #Shower Leakage
+                    if (prop_len > surface.len()):
+                        print("Shower Leakage")
+                        self.shower_state = np.delete(self.shower_state,self.size() - 1)
+                        self.shower_state = np.delete(self.shower_state,self.size() - 1)
 
-    def rem_part(self,indx: int):
-        """Clear particle from the shower (because it has left the material)
-        """
-        shower_state = np.delete(shower_state,indx)
+                    #Remove parent particle
+                    self.shower_state = np.delete(self.shower_state,0)
+                    part_indx += 1
+
+                elif(part[0] < 2*0.511):
+                    print("Photon is no longer showering")
+                    self.shower_state = np.delete(self.shower_state,0)
 
 
     def lepton_num(self):
         """Return lepton number from shower
         """
         lepnum = 0
-        for part in shower_state:
+        for part in self.shower_state:
             if abs(part[2] == 11):
-                lepnum += part[2]
+                lepnum += part[2]/11
+        return lepnum
 
-        return lepnum/11
+    def lepton_count(self):
+        """Return the total number of electrons and positrons in the shower
+        """
+        lepcount = 0
+        for part in self.shower_state:
+            if (part[2] == 11):
+                lepcount += 1
+        return lepcount
