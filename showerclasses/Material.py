95bad37f61eed34c761350ab322dc2f7c1e66979
dhumphreys1 <dhumphreys@umass.edu>
2022-12-05 13:53:26 -0500

    pre-committed repo

--- showerclasses/Material.py
+++ showerclasses/Material.py
@@ -1,46 +1,37 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Material:
-    """Material Object 
+    """Material Object"""
 
-    """
     length: float
     material: str
 
     def len(self):
-        """Return length of the material in cm
-        """
+        """Return length of the material in cm"""
         return self.length
 
     def dens(self):
-        """Retrun density of the material in g/cm^3
-        """
+        """Return density of the material in g/cm^3"""
         return 1
 
     def len_x0(self):
-        """Return length of material in units of radiation lengths
-        """
-        return self.length/self.radlen_cm() #Length in units of radiation length
+        """Return length of material in units of radiation lengths"""
+        return self.length / self.radlen_cm()  # Length in units of radiation length
 
-    def dist_to_x0(self, x : float):
-        """Return a distance in radiation lengths
-        """
-        return x/self.radlen_cm()
+    def dist_to_x0(self, x: float):
+        """Return a distance in radiation lengths"""
+        return x / self.radlen_cm()
 
     def radlen_cm(self):
-        """Return radiaiton length of material in cm
-        """
-        return 41.31 #Radiation Length in cm (polystyrene)
-    
+        """Return radiaiton length of material in cm"""
+        return 41.31  # Radiation Length in cm (polystyrene)
+
     def radlen(self):
-        """Return radiaiton length of material in g cm^-2
-        """
-        return 43.79 #g cm^-2
+        """Return radiaiton length of material in g cm^-2"""
+        return 43.79  # g cm^-2
 
     def e_crit(self):
-        """Return Critical Energy in MeV
-        """
-        return 93.11 #MeV (polystyrene)
-
-
+        """Return Critical Energy in MeV"""
+        return 93.11  # MeV (polystyrene)
--- showerclasses/Shower.py
+++ showerclasses/Shower.py
@@ -2,126 +2,118 @@ from dataclasses import dataclass
 from Material import Material
 import numpy as np
 
+
 @dataclass
 class Shower:
     """Shower Object
     shower_state = [(E,Pos,particle type/id)]
     """
-    surface : Material
-    initial_e : float #(in MeV)
+
+    surface: Material
+    initial_e: float  # (in MeV)
 
     def __post_init__(self):
-        self.shower_state = [(self.initial_e,0.0,11)]
+        self.shower_state = [(self.initial_e, 0.0, 11)]
         self.e_disp = 0
-    
+
     def size(self):
-        """Return the number of particles currently in the shower
-        """
+        """Return the number of particles currently in the shower"""
         return len(self.shower_state)
 
-
     def shower_surface(self):
-        """Return the surface object associated with the shower
-        """
+        """Return the surface object associated with the shower"""
         return self.surface
 
-
     def crnt_shower(self):
-        """Return current shower state
-        """
+        """Return current shower state"""
         return self.shower_state
 
-    def beta(self,E : float, mass = 0.511):
-        """Obtain the propagation speed of an electron/positron
-        """
-        return np.sqrt(1 - mass**2/E**2)
+    def beta(self, E: float, mass=0.511):
+        """Obtain the propagation speed of an electron/positron"""
+        return np.sqrt(1 - mass**2 / E**2)
 
-    def ionization_loss(self,delta_x : float, beta : float):
+    def ionization_loss(self, delta_x: float, beta: float):
         """Determine energy loss of an electron/positron due to ionization
         Here delta_x is taken to be in cm and dE/dx is assumed to be constant for showering leptons
         """
-        return self.surface.dens()*2*delta_x*1/beta**2
+        return self.surface.dens() * 2 * delta_x * 1 / beta**2
 
     def propagate(self):
-        """Propoagate shower
+        """Propagate shower
         Method will modify the shower object by propagating each of the constituent particles of the shower
         """
         part_indx = 0
         parent_size = self.size()
-        #for part in self.shower_state:
+        # for part in self.shower_state:
         for i in range(parent_size):
             part = self.shower_state[0]
-            if (abs(part[2]) == 11):
+            if abs(part[2]) == 11:
                 prop_len = self.surface.radlen()
-                if (abs(part[0] > self.surface.e_crit())):
-                    #print("Bremsstrahlung")
-                    self.shower_state.append((part[0]/2, part[1] + prop_len,part[2]))
-                    self.shower_state.append((part[0]/2, part[1] + prop_len,22))
+                if abs(part[0] > self.surface.e_crit()):
+                    # print("Bremsstrahlung")
+                    self.shower_state.append((part[0] / 2, part[1] + prop_len, part[2]))
+                    self.shower_state.append((part[0] / 2, part[1] + prop_len, 22))
 
-                    #Shower Leakage
-                    if (part[1] + prop_len > self.surface.len()):
+                    # Shower Leakage
+                    if part[1] + prop_len > self.surface.len():
                         print("Shower Leakage")
                         del self.shower_state[self.shower_state.size() - 1]
                         del self.shower_state[self.shower_state.size() - 1]
 
-                    #Ionization loss should be caluclated here so that shower leakage can be accounted for (if needed)
+                    # Ionization loss should be calculated here so that shower leakage can be accounted for (if needed)
 
-                    #Remove parent particle
+                    # Remove parent particle
                     del self.shower_state[0]
                     part_indx += 1
 
-                elif(part[0] < self.surface.e_crit()):
-                    #print("electron/positron is no longer showering")
+                elif part[0] < self.surface.e_crit():
+                    # print("electron/positron is no longer showering")
                     del self.shower_state[0]
 
                 continue
 
-            if (part[2] == 22):
+            if part[2] == 22:
                 prop_len = self.surface.radlen()
-                if(part[0] > 2*0.511):
-                    self.shower_state.append((part[0]/2, part[1] + prop_len,11))
-                    self.shower_state.append((part[0]/2, part[1] + prop_len,-11))
+                if part[0] > 2 * 0.511:
+                    self.shower_state.append((part[0] / 2, part[1] + prop_len, 11))
+                    self.shower_state.append((part[0] / 2, part[1] + prop_len, -11))
 
-                    #Shower Leakage
-                    if (part[1] + prop_len > self.surface.len()):
+                    # Shower Leakage
+                    if part[1] + prop_len > self.surface.len():
                         print("Shower Leakage")
                         del self.shower_state[self.shower_state.size() - 1]
                         del self.shower_state[self.shower_state.size() - 1]
 
-                    #Remove parent particle
+                    # Remove parent particle
                     del self.shower_state[0]
                     part_indx += 1
 
-                elif(part[0] <= 2*0.511):
-                    #print("Photon is no longer showering")
-                    del self.shower_state[0] 
-                                
-                continue
+                elif part[0] <= 2 * 0.511:
+                    # print("Photon is no longer showering")
+                    del self.shower_state[0]
 
+                continue
 
     def lepton_num(self):
-        """Return lepton number from shower
-        """
+        """Return lepton number from shower"""
         lepnum = 0
         for part in self.shower_state:
-            if (abs(part[2]) == 11):
-                lepnum += part[2]/11
+            if abs(part[2]) == 11:
+                lepnum += part[2] / 11
         return int(lepnum)
 
     def lepton_count(self):
-        """Return the total number of electrons and positrons in the shower
-        """
+        """Return the total number of electrons and positrons in the shower"""
         lepcount = 0
         for part in self.shower_state:
-            if (abs(part[2]) == 11):
+            if abs(part[2]) == 11:
                 lepcount += 1
         return lepcount
 
     def photon_count(self):
-        """Return the total number of electrons and positrons in the shower
-        """
+        """Return the total number of electrons and positrons in the shower"""
         photo_count = 0
         for part in self.shower_state:
-            if (part[2] == 22):
+            if part[2] == 22:
                 photo_count += 1
         return photo_count
