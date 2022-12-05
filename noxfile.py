8fc8fc1b2adcdeebc9f86085010676a60473b3c7
Elise Chavez <elisechavez23@gmail.com>
2022-12-05 15:26:40 -0600

    docs stuff

--- noxfile.py
+++ noxfile.py
@@ -19,7 +19,7 @@ def docs(session: nox.Session) -> None:
 
     session.install(".[docs]")
     session.chdir("docs")
-    session.run("sphinx-build", "-M", "html", ".", "build")
+    session.run("sphinx-build", "-M", "html", "./source", "docs/build")
 
 
 @nox.session
