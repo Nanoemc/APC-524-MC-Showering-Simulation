260e6f2c840a94fa3da8993655581339b69ff8f6
Elise Chavez <elisechavez23@gmail.com>
2022-12-05 14:49:45 -0600

    noxfile

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath("."))
-
+sys.path.insert(0, os.path.abspath("../.."))
 # -- Project information -----------------------------------------------------
 
 project = "APC-524-MC-Showering-Simulation"
