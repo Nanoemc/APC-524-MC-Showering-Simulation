f309494fb3ce30b0fc917b1d0e48c3b8f425f03e
Elise Chavez <elisechavez23@gmail.com>
2022-12-05 15:50:06 -0600

    fixed noxfile, undid an edit

--- noxfile.py
+++ noxfile.py
@@ -19,7 +19,7 @@ def docs(session: nox.Session) -> None:
 
     session.install(".[docs]")
     session.chdir("docs")
-    session.run("sphinx-build", "-M", "html", "./source", "docs/build")
+    session.run("sphinx-build", "-M", "html", ".", "build")
 
 
 @nox.session
