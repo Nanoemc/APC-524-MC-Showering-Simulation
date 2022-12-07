844762301fbf874f18f04bb667672185901181f4
twnelson0 <43486832+twnelson0@users.noreply.github.com>
2022-12-07 09:50:05 -0600

    Merge my branch with all changes made in main (#2)
    * docs
    
    * Create make_the_docs.yml
    
    * Delete make_the_docs.yml
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * adding docs for real hopefully
    
    * adding docs for real hopefully
    
    * trying to fix the noxfile not found error
    
    * trying to fix the noxfile not found error
    
    * fixing noxfile not found
    
    * noxfile
    
    * noxfile
    
    * docs stuff
    
    * added checkout
    
    * fixed noxfile, undid an edit
    
    * fixed noxfile, undid an edit
    
    * fixing docs
    
    * fixing docs
    
    * 404 error
    
    * 404 error
    
    * index.html?
    
    * index.html? part 2
    
    * index....
    
    * index....
    
    * index....
    
    * docs attempt
    
    Co-authored-by: Elise Chavez <elisechavez23@gmail.com>
    Co-authored-by: Elise Chavez <59752274+Nanoemc@users.noreply.github.com>

---
+++ docs/source/index.html
@@ -0,0 +1,21 @@
+.. APC-524-MC-Showering-Simulation documentation master file, created by
+   sphinx-quickstart on Mon Dec  5 13:56:33 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to APC-524-MC-Showering-Simulation's documentation!
+===========================================================
+
+.. toctree::
+   :maxdepth: 2
+
+   usage/installation
+   usage/quickstart
+   ...
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- noxfile.py
+++ noxfile.py
@@ -9,3 +9,21 @@ def tests(session):
     session.install(".[test]")
     session.install("pytest")
     session.run("pytest", *session.posargs)
+
+
+@nox.session
+def docs(session: nox.Session) -> None:
+    """
+    Build the docs. Pass "--serve" to serve.
+    """
+
+    session.install(".[docs]")
+    session.chdir("docs")
+    session.run("sphinx-build", "-M", "html", "./source", "build")
+
+
+@nox.session
+def serve(session: nox.Session) -> None:
+    docs(session)
+    print("Launching docs at http://localhost:8000/ - use Ctrl-C to quit")
+    session.run("python", "-m", "http.server", "8000", "-d", "_build/html")
